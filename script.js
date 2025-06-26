6df923e5ed09db73f5cec9af1d36836008956ef9
frixc7 <frixc7@gmail.com>
2025-06-26 15:46:52 +0100

    Create 16x16 grid
    - Main container
    - 16 column divs inside the container
    - 16 pixel divs inside each column

---
+++ script.js
@@ -0,0 +1,17 @@
+const container = document.querySelector(".container");
+
+if (container) {
+    for (let i = 0; i <= 16; i++) {
+        var column = document.createElement("div");
+        column.classList.add("column");
+        container.appendChild(column);
+        if (column) {
+            for (let i = 0; i <= 16; i++) {
+                var pixel = document.createElement("div");
+                pixel.classList.add("pixel");
+                column.appendChild(pixel);
+            }
+        }
+    }
+}
+
